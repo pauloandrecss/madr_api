0b29fbd8ce69e4e0d42160c4fb4c0f029bb81ecd
Paulo André Cardoso Silva Santos <pauloandrecss@gmail.com>
2024-08-23 23:11:39 -0300

    Configuração do nosso banco de dados

---
+++ backend/database.py
@@ -0,0 +1,9 @@
+from backend.settings import Settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+engine = create_engine(Settings().DATABASE_URL)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
